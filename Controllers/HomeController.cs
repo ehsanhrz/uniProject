c124fc9463cc0de7953a400f617547f841806d2a
ehsan_hrz <ehsanheidarzadeh959@gmail.com>
2023-06-15 17:40:07 +0330

    setting new files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,20 +7,66 @@ namespace uniProject.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
+    private readonly FormContext _context;
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _context = new FormContext();
+
     }
 
-    public IActionResult Index()
+    [HttpGet("{trackingId:long?}")]
+    public IActionResult Index(long? trackingId)
     {
-        return View();
+        if (trackingId == null)
+        {
+            return View(new MainFormModel());
+        }
+      
+        var result = _context.Forms.FirstOrDefault(item => item.TrackingId == trackingId);
+        if (result == null)
+        {
+            return View(new MainFormModel());
+        }
+        return View(result);
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult Index(MainFormModel formModel)
     {
-        return View();
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(formModel);
+            }
+            var searchResult = _context.Forms
+                .FirstOrDefault(item => item.TrackingId == formModel.TrackingId);
+            if (searchResult == null || formModel.TrackingId == null)
+            {
+                formModel.TrackingId = DateTime.Now.Ticks;
+                _context.Forms.Add(formModel);
+            }
+            else
+            {
+                _context.Forms.Update(formModel);    
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Success",new{trackingId = formModel.TrackingId});
+        }
+        catch(Exception ex)
+        {
+            ModelState.AddModelError("",ex.Message);
+            return View(formModel);
+        }
+       
+    }
+
+    [HttpGet("{trackingId:long}")]
+    public IActionResult Success(long? trackingId)
+    {
+        
+        return View(trackingId);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
